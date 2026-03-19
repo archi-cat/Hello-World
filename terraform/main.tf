004cbd8584ed258a5f7a43e93e88dccb26dbaabe
Flo Ryda <archi_cat@yahoo.com>
2026-03-19 19:10:41 +0000

    change the SKU for the app svc plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_service_plan" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   os_type             = "Linux"
-  sku_name            = "B1"  # Basic tier — cheapest paid tier that supports containers
+  sku_name            = "P0V4"  # Basic tier — cheapest paid tier that supports containers
 }
 
 # ── App Service (Web App for Containers) ─────────────────────────────────────
